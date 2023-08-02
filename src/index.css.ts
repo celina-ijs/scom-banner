4cd70cbf594ff63818d0d4adab1512457e9738c1
johnnycheukijs <johnnycheuk@instajs.com>
2023-08-02 14:44:38 +0800

    Set overflow to hidden

--- src/index.css.ts
+++ src/index.css.ts
@@ -4,9 +4,10 @@ const Theme = Styles.Theme.ThemeVars;
 export const containerStyle = Styles.style({
   width: 'var(--layout-container-width)',
   maxWidth: 'var(--layout-container-max_width)',
-  overflow: 'var(--layout-container-overflow)',
+  // overflow: 'var(--layout-container-overflow)',
+  overflow: 'hidden',
   textAlign: ('var(--layout-container-text_align)' as any),
-  margin: '0 auto'
+  margin: '0 auto',
 })
 
 export const backgroundStyle = Styles.style({
